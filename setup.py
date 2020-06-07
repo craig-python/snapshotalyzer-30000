e0833027fee9da42ee6258d0dac8a6048ff99962
Craig Dalrymple <craig.dalrymple@btinternet.com>
2020-06-07 17:34:36 +0100

    Setup.py to enable build and distribution of shotty

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='snapshotalyzer-30000',
+    version='0.1',
+    author="Craig Dalrymple",
+    author_email="craig.dalrymple91@gmail.com",
+    description="SnapshotAlyzer 30000 is a tool to manage EC2 snapshots. Created during Robin Norwood's Python course on acloudguru",
+    license="GPLv3+",
+    packages=['shotty'],
+    url="https://github.com/craig-python/snapshotalyzer-30000",
+    install_requires=[
+        'click',
+        'boto3'
+    ],
+    entry_points='''
+        [console_scripts]
+        shotty=shotty.shotty:cli
+    ''',
+)
